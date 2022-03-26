c5a2b65c1a0074eb1e39f03f5680783f0cd4f54d
Vinsyahputra <vinthecyborg@gmail.com>
2022-03-26 08:12:22 +0700

    fixing error bag 3 suku kata & akhiran (done)

--- Assets/Scripts/TigaSukuKata.cs
+++ Assets/Scripts/TigaSukuKata.cs
@@ -7,9 +7,14 @@ public class TigaSukuKata : MonoBehaviour
 	private Animator animator;
 	
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+   //  	animator = GetComponent<Animator>();
+
+   //       if (animator.GetCurrentAnimatorStateInfo(0).IsName("3SukuKata"))
+		 // {
+		 //    animator.enabled = false;
+		 // }
     }
 
     // Update is called once per frame
@@ -18,8 +23,9 @@ public class TigaSukuKata : MonoBehaviour
         
     }
 
-    void OnMouseDown(){ //down
+    void OnMouseDown(){ 
     	animator = GetComponent<Animator>();
     	animator.Play("3SukuKata");
+    	GetComponent<AudioSource>().Play();
     }
 }
