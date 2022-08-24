f7346382bc6a4436ec346e255a9b73b4aaafc02d
Vinsyahputra <vinthecyborg@gmail.com>
2022-08-24 19:25:51 +0700

    update materi ng ny

---
+++ Assets/Scripts/ngny_click_animate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ngny_click_animate : MonoBehaviour
+{
+    float x,y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = transform.localScale.x;
+        y = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown(){
+    	
+    	GetComponent<AudioSource>().Play();
+    	LeanTween.scale(gameObject, new Vector2(x * 1.8f, y * 1.8f), .5f);
+    	// transform.localScale = new Vector2(x * 1.2f, y*1.2f);
+    }
+
+    void OnMouseUp(){
+    	// transform.localScale = new Vector2(x,y);
+    	LeanTween.scale(gameObject, new Vector2(x, y), .25f).setDelay(.5f);
+    }
+}
